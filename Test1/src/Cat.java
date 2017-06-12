421d0c246de2cb50e207d593bc9f8a716a0d4e2e
zdlulu <329433319@qq.com>
2017-06-12 17:02:05 +0800

    随便写的练习sourcetree的使用

---
+++ Test1/src/Cat.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2017/6/12.
+ */
+public class Cat {
+
+    int age;
+    String name;
+    String color;
+}
--- Test1/src/Main.java
+++ Test1/src/Main.java
@@ -1,6 +1,16 @@
+import java.time.temporal.ValueRange;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("22222222222");
+        Cat cat1 = new Cat();
+        cat1.age = 1;
+        cat1.name = "小白";
+        cat1.color = "白色";
+
+        Cat cat2 = new Cat();
+        cat2.age = 2;
+        cat2.name = "小灰";
+        cat2.color = "灰色";
     }
 }
