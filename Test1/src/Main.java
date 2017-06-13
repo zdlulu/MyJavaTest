5ba0642fdee8adc455eac9abcb6af7c3d04e7ce2
zdlulu <329433319@qq.com>
2017-06-13 17:10:02 +0800

    修改版本的等腰三角形的输出

--- Test1/src/Main.java
+++ Test1/src/Main.java
@@ -1,6 +1,6 @@
 /*程序建立时间:2017/06/13
   主题：输入一个数比如说是4，
-  使用*画倒直线只画边缘线不画内容线和上一个例子是不同的*/
+  使用*画倒直线只画边缘线不画内容线和上一个例子是不同的,等腰三角形的轮廓*/
 import java.util.Scanner;
 
 public class Main {
@@ -12,8 +12,11 @@ public class Main {
         System.out.println("倒直线设置为:"+input_num);
 
         for(int i=0;i<input_num;i++){
-            for(int j=input_num;j>=0;j--){
-                if(i==j){
+
+            for(int j=0;j<2*input_num;j++){
+                if(j==(input_num-i)){
+                    System.out.print("*");
+                }else if(j==(i+input_num)){
                     System.out.print("*");
                 }else{
                     System.out.print(" ");
