967a8df9b0d074bd5234ca8c9463ef5bbb357810
zdlulu <329433319@qq.com>
2017-06-13 17:33:39 +0800

    自定义截取字符串的方法

--- Test1/src/Main.java
+++ Test1/src/Main.java
@@ -1,29 +1,46 @@
 /*程序建立时间:2017/06/13
-  主题：输入一个数比如说是4，
-  使用*画倒直线只画边缘线不画内容线和上一个例子是不同的,等腰三角形的轮廓*/
+  主题：自定义截取字符串的方法，
+  使用：通过输入两个index的值，来截取某一个字符串的部分内容，并输出结果*/
 import java.util.Scanner;
+import java.util.logging.Level;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        /********************************************/
         Scanner sc = new Scanner(System.in);
-        System.out.println("直线行数:");
-        int input_num = Integer.parseInt(sc.nextLine());
-        System.out.println("倒直线设置为:"+input_num);
-
-        for(int i=0;i<input_num;i++){
-
-            for(int j=0;j<2*input_num;j++){
-                if(j==(input_num-i)){
-                    System.out.print("*");
-                }else if(j==(i+input_num)){
-                    System.out.print("*");
-                }else{
-                    System.out.print(" ");
-                }
-            }
-            System.out.println("");
-        }
-
+        System.out.println("请输入字符串:");
+        String input_str = sc.nextLine();
+        System.out.println("输入的字符串为:"+input_str);
+        /********************************************/
+        System.out.println("请输入开始位置:");
+        int input_location = Integer.parseInt(sc.nextLine());
+        System.out.println("输入开始位置为:"+input_location);
+        /********************************************/
+        System.out.println("请输入转化长度:");
+        int input_len = Integer.parseInt(sc.nextLine());
+        System.out.println("输入转化长度为:"+input_len);
+        /********************************************/
+        String result = cut_str(input_str,input_location,input_len);
+        System.out.println("最终的转化结果为:"+result);
+        /********************************************/
     }
+
+    private static String cut_str(String str, int begin, int length){
+        String result = "";
+        if(str!=null){
+            byte[] str_byte = str.getBytes();
+            int len_byte = str_byte.length;
+            if((begin+length)<=len_byte){
+                result = new String(str_byte,0,length);
+            }else{
+                System.out.println("字符串长度输入的有问题!!!!");
+            }
+        }else{
+            System.out.println("字符串输入为空!!!!");
+        }
+        return result;
+    }
+
 }
