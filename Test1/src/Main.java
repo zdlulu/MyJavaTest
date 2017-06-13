1887ca71577427a0592d2ad8efe4359869614a02
zdlulu <329433319@qq.com>
2017-06-13 15:47:30 +0800

    一个*的半圆图形，没有做成整圆

--- Test1/src/Main.java
+++ Test1/src/Main.java
@@ -1,27 +1,29 @@
 /*程序建立时间:2017/06/13
   主题：输入一个数比如说是4，
-  等腰三角形显示星星，行数为4*/
+  使用*图标输出半径为4的圆*/
 
 
-import java.security.Principal;
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("星星行数:");
-        int num_star = Integer.parseInt(sc.nextLine());  //输入的是最后一行星星的个数
-        System.out.println("星星行数设置为:"+num_star);
+        System.out.println("半径:");
+        int ir = Integer.parseInt(sc.nextLine());
+        System.out.println("半径设置为:"+ir);
 
-        //采用的方式是，每一行输出后进行换行后形成所需要的图形
-        for (int i = 0; i < num_star; i++) {
-            //需要的空格部分进行输出，空格部分是递减的等差数列，排列的
-            for(int j=0;j<num_star-i-1;j++){
-                System.out.print("&");
-            }
-            //每一行的星星数量是等差数列排列的
-            for(int j=0;j<2*i+1;j++){
+        double r = ir;
+
+        for(double x=0;x<=r*2;x++){
+
+            DecimalFormat df = new DecimalFormat("#.00");//保留两位小数
+            double y = r+Math.sqrt(Math.pow(r,2)-Math.pow((x-r),2));
+//            System.out.println("y="+df.format(y));
+//            System.out.println("x="+df.format(x));
+
+            for(int j=0;j<y;j++){
                 System.out.print("*");
             }
             System.out.println();
