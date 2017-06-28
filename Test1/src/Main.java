a43622e0dbdcd2153e78f0c49ae02ce4782d92b3
zdlulu <329433319@qq.com>
2017-06-28 16:41:11 +0800

    重载:是指一个类中出现多个方法名相同，但参数个数或参数类型不同的方法称之为方法的重载

--- Test1/src/Main.java
+++ Test1/src/Main.java
@@ -1,19 +1,23 @@
 /*程序建立时间:2017/06/28
-  主题：继承与重写
-  输出：继承遵循父类的规则，重写后按照重写后的方法内容执行
+  主题：重载
+  输出：是指一个类中出现多个方法名相同，但参数个数或参数类型不同的方法
+       称之为方法的重载
 */
-
 public class Main {
 
     public static void main(String[] args) {
-        Dog dog = new Dog();
-        System.out.print("Dog的结果:");
-        dog.cry();
-        dog.name();
-        System.out.println("/.../.../.../.../");
-        Sheep sheep = new Sheep();
-        System.out.print("Sheep的结果:");
-        sheep.cry();
-        sheep.name();
+        Calculate calculate = new Calculate();
+        float l = 20;
+        float w = 30;
+        float a_r = calculate.getArea(l,w);
+        System.out.println("求长为:"+l+"宽为"+w+"的矩形面积是:"+a_r);
+
+        float r =7;
+        float a_c = calculate.getArea(r);
+        System.out.println("求半径为:"+r+"的圆形面积是:"+a_c);
+
+        int num = 7;
+        calculate.draw(num);
+        calculate.draw("三角形");
     }
 }
