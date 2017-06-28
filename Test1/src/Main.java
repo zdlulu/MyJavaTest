99e3d3454de73f864fa7e91afba833445c4d3764
zdlulu <329433319@qq.com>
2017-06-28 17:26:36 +0800

    闹钟的程序，没有明白，直接抄袭的源码

--- Test1/src/Main.java
+++ Test1/src/Main.java
@@ -1,26 +1,15 @@
-/*程序建立时间:2017/06/28
-  输出：使用comparable接口自定义排序
-*/
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import javax.swing.*;
 
-
+/**
+ * Created by Administrator on 2017/6/28.
+ * 输出：闹钟的程序，没有明白，直接抄袭的源码
+ */
 public class Main {
 
-    public static void main(String[] args) {
-        List<Employee> list=new ArrayList<Employee>();
-        list.add(new Employee(3,"Java",21));
-        list.add(new Employee(2,"Java",22));
-        list.add(new Employee(1,"Java",23));
-        System.out.println("排序前：");
-        for (Employee employee : list) {
-            System.out.println(employee);
-        }
-        System.out.println("排序后：");
-        Collections.sort(list);				//执行自动排序
-        for (Employee employee : list) {
-            System.out.println(employee);
-        }
+    public static void main(String[] args){
+        AlarmClock clock = new AlarmClock(1000, true);
+        clock.start();
+        JOptionPane.showMessageDialog(null, "是否退出？");
+        System.exit(0);
     }
 }
