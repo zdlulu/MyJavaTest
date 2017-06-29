5442d65c034b6f589c0a105b82f67cb02b5f9eb6
zdlulu <329433319@qq.com>
2017-06-29 15:12:53 +0800

    no message

--- Test1/src/Main.java
+++ Test1/src/Main.java
@@ -1,17 +1,6 @@
-/*Created by Administrator on 2017/6/29.
- *内容：Swing常用窗体的练习，添加了一个jLabel的控件
- */
+public class Main {
 
-import java.util.Scanner;
-
-public class Main  {
-
-    public static void main(String[] args){
-        System.out.print("请输入窗体的title:");
-        Scanner in = new Scanner(System.in);
-        String title = in.nextLine();
-        CreateJFrame cjf = new CreateJFrame();
-        cjf.CreateJFrame(title);
-
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
